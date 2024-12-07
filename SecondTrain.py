c05f9b41c0965854f55829ada98b69a4243a56b2
S1098357 <s1098357@studenti.univpm.it>
2024-12-07 15:02:54 +0100

    Preprocessing di alcune colonne che davano errore

--- SecondTrain.py
+++ SecondTrain.py
@@ -1,23 +1,32 @@
 from pytorch_tabnet.tab_model import TabNetClassifier
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
 # Carica il modello salvato
 model = TabNetClassifier()
 model.load_model("tabnet_model2.zip")
 
 # Carica e prepara il nuovo dataset
-new_data = pd.read_csv("../Dataset1/Friday-16-02-2018_TrafficForML_CICFlowMeter.csv")
+new_data = pd.read_csv("../Dataset1/Friday-16-02-2018_TrafficForML_CICFlowMeter.csv", low_memory=False)
 new_data = new_data.replace([np.inf, -np.inf], np.nan)
+non_numeric_columns = new_data.select_dtypes(include=['object', 'category']).columns
+
+for col in non_numeric_columns:
+    try:
+        new_data[col] = pd.to_numeric(new_data[col], errors='coerce')
+    except Exception as e:
+        print(f"Errore nella conversione di {col}: {e}")
+
 new_data = new_data.fillna(0)  # Sostituisci con la strategia di preprocessamento usata in precedenza
 
 # Separa le feature (X) e le label (y)
 new_data = new_data.drop(columns=["Timestamp"])
-X_new = new_data.drop(columns=["Label"])
 y_new = new_data["Label"]
+X_new = new_data.drop(columns=["Label"])
+
 
 # Converte le etichette in numeri se necessario (ad esempio, categorizzazione)
-from sklearn.preprocessing import LabelEncoder
 encoder = LabelEncoder()
 y_new = encoder.fit_transform(y_new)
 
@@ -29,8 +38,8 @@ model.fit(
     patience=10,           # Early stopping
     batch_size=1024,       # Dimensione del batch
     virtual_batch_size=128,  # Virtual batch size
-    from_unsupervised=model # Indica che è supervisato
+    from_unsupervised=model  # Indica che è supervisato
 )
 
 # Salva il modello ri-addestrato
-model.save_model("tabnet_model3.zip")
+model.save_model("tabnet_model3")
