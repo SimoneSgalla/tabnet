45cdc80b328be58655b27d125f937a89f74b0b9d
S1098357 <s1098357@studenti.univpm.it>
2024-12-08 10:56:26 +0100

    Preprocessing di alcune colonne che davano errore

--- SecondTrain.py
+++ SecondTrain.py
@@ -1,14 +1,15 @@
 from pytorch_tabnet.tab_model import TabNetClassifier
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 # Carica il modello salvato
 model = TabNetClassifier()
-model.load_model("tabnet_model2.zip")
+model.load_model("tabnet_model3.zip")
 
 # Carica e prepara il nuovo dataset
-new_data = pd.read_csv("../Dataset1/Friday-16-02-2018_TrafficForML_CICFlowMeter.csv", low_memory=False)
+new_data = pd.read_csv("../Dataset1/Thursday-01-03-2018_TrafficForML_CICFlowMeter.csv", low_memory=False)
 new_data = new_data.replace([np.inf, -np.inf], np.nan)
 non_numeric_columns = new_data.select_dtypes(include=['object', 'category']).columns
 
@@ -24,6 +25,8 @@ new_data = new_data.fillna(0)  # Sostituisci con la strategia di preprocessament
 new_data = new_data.drop(columns=["Timestamp"])
 y_new = new_data["Label"]
 X_new = new_data.drop(columns=["Label"])
+X_new = X_new.clip(lower=-1e10, upper=1e10)
+X = X_new.fillna(X_new.mean())
 
 
 # Converte le etichette in numeri se necessario (ad esempio, categorizzazione)
@@ -31,15 +34,29 @@ encoder = LabelEncoder()
 y_new = encoder.fit_transform(y_new)
 
 # Ri-Addestra il modello
-model.fit(
+'''model.fit(
     X_train=X_new.values,  # Assicurati di convertire in NumPy array
     y_train=y_new,
     max_epochs=50,         # Numero di epoche da configurare
     patience=10,           # Early stopping
     batch_size=1024,       # Dimensione del batch
     virtual_batch_size=128,  # Virtual batch size
-    from_unsupervised=model  # Indica che è supervisato
+    from_unsupervised=model,  # Indica che è supervisato
+)'''
+
+X_train, X_test, y_train, y_test = train_test_split(X_new, y_new, test_size=0.2, random_state=42)
+
+model.fit(
+    X_train=X_train.values,
+    y_train=y_train,
+    eval_set=[(X_test.values, y_test)],
+    eval_metric=['accuracy'],
+    max_epochs=50,         # Numero di epoche da configurare
+    patience=10,           # Early stopping
+    batch_size=1024,       # Dimensione del batch
+    virtual_batch_size=128,  # Virtual batch size
+    from_unsupervised=model,  # Indica che è supervisato
 )
 
 # Salva il modello ri-addestrato
-model.save_model("tabnet_model3")
+model.save_model("tabnet_model4")
